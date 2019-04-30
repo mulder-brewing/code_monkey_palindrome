94076b0edc236ac33fa00ec2bfe4c289d97cc4a1
Code Monkey <code.monkey@codemonkey.com>
2019-04-30 15:02:09 -0400

    Initialize repository

---
+++ lib/code_monkey_palindrome.rb
@@ -0,0 +1,6 @@
+require "code_monkey_palindrome/version"
+
+module CodeMonkeyPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/code_monkey_palindrome/version.rb
@@ -0,0 +1,3 @@
+module CodeMonkeyPalindrome
+  VERSION = "0.1.0"
+end
